56b6c7234482ae752857b43b707d95fd5eb3a89a
eder <edpefal@gmail.com>
2015-11-09 09:18:15 -0600

    Push Broadcast

---
+++ app/src/main/java/com/wezen/madisonpartner/request/IncomingRequestBroadcastReceiver.java
@@ -0,0 +1,34 @@
+package com.wezen.madisonpartner.request;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by eder on 11/7/15.
+ */
+public class IncomingRequestBroadcastReceiver extends com.parse.ParsePushBroadcastReceiver {
+
+    private static String PARSE_DATA_KEY = "homeServiceRequest";
+
+    @Override
+    protected void onPushReceive(Context context, Intent intent) {
+        super.onPushReceive(context, intent);
+        //getDataFromIntent(intent);
+    }
+
+    private JSONObject getDataFromIntent(Intent intent) {
+        JSONObject data = null;
+        try {
+            data = new JSONObject(intent.getExtras().getString(PARSE_DATA_KEY));
+            Log.d("TAG","data: " + data);
+        } catch (JSONException e) {
+            // Json was not readable...
+
+        }
+        return data;
+    }
+}
